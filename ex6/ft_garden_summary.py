53272c2406b3d17180e8b852795fe5f36e12356b
ac-kavali <ahmadachahi400@gmail.com>
2026-01-09 00:50:31 +0100

    projet en cours

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,8 @@
+def ft_garden_summary():
+    garden_name = input("Enter garden name: ")
+    n_plants = int(input("Enter number of plants: "))
+    print (f"Garden: {garden_name}")
+    print (f"Plants: {n_plants}")
+    print ("Status: Growing well!")
+
+ft_garden_summary()
