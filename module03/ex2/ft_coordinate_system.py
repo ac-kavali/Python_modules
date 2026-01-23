f23bc5c91379eb9d975b149268103668a279a463
ac-kavali <ahmadachahi400@gmail.com>
2026-01-23 14:04:27 +0100

    new changes

--- module03/ex2/ft_coordinate_system.py
+++ module03/ex2/ft_coordinate_system.py
@@ -6,20 +6,28 @@ def calculate_distance(p1, p2):
     x1, y1, z1 = p1
     x2, y2, z2 = p2
 
-    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+    distance = math.sqrt(
+        (x2 - x1) ** 2
+        + (y2 - y1) ** 2
+        + (z2 - z1) ** 2
+    )
     return distance
 
 
 def parse_coordinates(arg, coords):
     try:
-        numbers_str = arg.split("(")[1].split(")")[0]  #this called chained method calls
+        numbers_str = arg.split("(")[1].split(")")[0]  # chained method calls
         numbers_list = numbers_str.split(", ")
         coords = tuple(int(x) for x in numbers_list)
         return coords
     except ValueError as e:
         print(f"Error parsing coordinates: {e}")
-        print(f'Error details - Type: {type(e).__name__} Args: ("{e}")')
+        print(
+            f'Error details - Type: {type(e).__name__} '
+            f'Args: ("{e}")'
+        )
         return coords
+
 
 def unpacking_demonstration(latest_coords):
     print("\nUnpacking demonstration:")
@@ -28,10 +36,9 @@ def unpacking_demonstration(latest_coords):
     print(f"Coordinates: X={x}, Y={y}, Z={z}")
 
 
-
 coordinates = initial_position = (0, 0, 0)
 
-# parsing the coordinates giving in command line
+# Parsing the coordinates given in command line
 coordinates = parse_coordinates(sys.argv[1], coordinates)
 print("=== Game Coordinate System ===")
 print(f"\nPosition created: {coordinates}")
@@ -41,7 +48,7 @@ print(
     f"{calculate_distance(initial_position, coordinates):.1f}"
 )
 
-#parsing new input coordinates and calculate distance
+# Parsing new input coordinates and calculate distance
 coordinates = parse_coordinates(input("\nParsing coordinates:"), coordinates)
 print(f"Parsed position: {coordinates}")
 print(
@@ -50,9 +57,8 @@ print(
     f"{calculate_distance(initial_position, coordinates):.1f}"
 )
 
-
 print('\nParsing invalid coordinates: "abc,def,ghi"')
 coordinates = parse_coordinates("(aa, bb, cc)", coordinates)
 
-#unpacking the demonstration
+# Unpacking demonstration
 unpacking_demonstration(coordinates)
