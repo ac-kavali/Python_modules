3295c4ce8fd06988aa35025bad37922de0f8105d
ac-kavali <ahmadachahi400@gmail.com>
2026-01-24 23:08:46 +0100

    something new

--- module04/ex1/ft_archive_creation.py
+++ module04/ex1/ft_archive_creation.py
@@ -7,14 +7,14 @@ print("Storage unit created successfully...\n")
 print("Inscribing preservation data...")
 
 data = [
-"{[}ENTRY 001{]} New quantum algorithm discovered\n",
-"{[}ENTRY 002{]} Efficiency increased by 347%\n"
-"{[}ENTRY 003{]} Archived by Data Archivist trainee\n"
+    "{[}ENTRY 001{]} New quantum algorithm discovered\n",
+    "{[}ENTRY 002{]} Efficiency increased by 347%\n"
+    "{[}ENTRY 003{]} Archived by Data Archivist trainee\n"
 ]
 
 for line in data:
     file.write(line)
-    print(line,end="")
+    print(line, end="")
 
 file.close()
 
--- module04/ex2/ft_stream_management.py
+++ module04/ex2/ft_stream_management.py
@@ -2,8 +2,14 @@ import sys
 
 print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n")
 ar_ID = input("Input Stream active. Enter archivist ID: ")
-print("Input Stream active. Enter status report:", end = "")
+print("Input Stream active. Enter status report: ", end="")
 status_rep = sys.stdin.readline()
 
-sys.stdout.write(f'{{]}}STANDARD {{]}} Archive status from: {ar_ID}: {status_rep}\n')
-sys.stderr.write(f'{{[}}ALERT{{]}} System diagnostic: Communication channels verified\n')
+sys.stdout.write(f'\n{{]}}STANDARD{{]}} Archive status from: '
+                 f'{ar_ID}: {status_rep}\n')
+word = "{[}ALERT{]}"
+sys.stderr.write(f'{word} System diagnostic: '
+                 f'Communication channels verified\n')
+
+print("{{]}}STANDARD {{]}} Data transmission complete\n")
+print("Three-channel communication test successful.")
