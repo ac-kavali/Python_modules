4c5f135e4c370893af238ee1d16d8becc8a99bfb
ac-kavali <ahmadachahi400@gmail.com>
2026-01-25 00:03:37 +0100

    something new

--- module04/ex0/ft_ancient_text.py
+++ module04/ex0/ft_ancient_text.py
@@ -1,7 +1,7 @@
 print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===")
 print("\nAccessing Storage Vault: ancient_fragment.txt")
 
-file = open("/home/achahi/PyCharmMiscProject/Python_modules/module04/files/ancient_fragment.txt", "r")
+file = open("ancient_fragment.txt", "r")
 print("Connection established...")
 print("\nRECOVERED DATA:")
 
