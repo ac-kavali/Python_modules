64192da752404d00361fe214f66bde4c6bc0f1d4
ac-kavali <ahmadachahi400@gmail.com>
2026-01-24 21:57:47 +0100

    something new

--- module04/ex1/ft_archive_creation.py
+++ module04/ex1/ft_archive_creation.py
@@ -1,2 +1,22 @@
 print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===")
-print("hello")
+print("\nInitializing new storage unit: new_discovery.txt")
+
+file = open("new_discovery.txt", "w")
+
+print("Storage unit created successfully...\n")
+print("Inscribing preservation data...")
+
+data = [
+"{[}ENTRY 001{]} New quantum algorithm discovered\n",
+"{[}ENTRY 002{]} Efficiency increased by 347%\n"
+"{[}ENTRY 003{]} Archived by Data Archivist trainee\n"
+]
+
+for line in data:
+    file.write(line)
+    print(line,end="")
+
+file.close()
+
+print("\nData inscription complete. Storage unit sealed.")
+print("Archive 'new_discovery.txt' ready for long-term preservation.")
