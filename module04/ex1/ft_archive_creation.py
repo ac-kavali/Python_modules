0116c84e31aef4565db85a1a87487a164fbeddc6
ac-kavali <ahmadachahi400@gmail.com>
2026-01-25 15:07:31 +0100

    something new

--- module04/ex1/ft_archive_creation.py
+++ module04/ex1/ft_archive_creation.py
@@ -7,9 +7,9 @@ print("Storage unit created successfully...\n")
 print("Inscribing preservation data...")
 
 data = [
-    "{[}ENTRY 001{]} New quantum algorithm discovered\n",
-    "{[}ENTRY 002{]} Efficiency increased by 347%\n"
-    "{[}ENTRY 003{]} Archived by Data Archivist trainee\n"
+    "[ENTRY 001] New quantum algorithm discovered\n",
+    "[ENTRY 002] Efficiency increased by 347%\n"
+    "[ENTRY 003] Archived by Data Archivist trainee\n"
 ]
 
 for line in data:
--- module04/ex2/ft_stream_management.py
+++ module04/ex2/ft_stream_management.py
@@ -1,15 +1,21 @@
 import sys
 
 print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n")
+
+# Read Entry Using "input()"
 ar_ID = input("Input Stream active. Enter archivist ID: ")
 print("Input Stream active. Enter status report: ", end="")
+
+# Read Entry Using "sys.stdin"
 status_rep = sys.stdin.readline()
 
-sys.stdout.write(f'\n{{]}}STANDARD{{]}} Archive status from: '
+# Print Out using sys.stdout Entry
+sys.stdout.write(f'\n[STANDARD] Archive status from: '
                  f'{ar_ID}: {status_rep}\n')
-word = "{[}ALERT{]}"
-sys.stderr.write(f'{word} System diagnostic: '
-                 f'Communication channels verified\n')
+
+# Print and test Diagnostic (Error) channel
+sys.stderr.write('[ALERT System diagnostic: '
+                 'Communication channels verified\n')
 
 print("{{]}}STANDARD {{]}} Data transmission complete\n")
 print("Three-channel communication test successful.")
