8051b9b562de78a18301488dc0e810cc9de04dc5
ac-kavali <ahmadachahi400@gmail.com>
2026-01-31 10:51:46 +0100

    update

--- module07/ex2/main.py
+++ module07/ex2/main.py
@@ -9,4 +9,4 @@ print("- Combatable: ['attack', 'defend', 'get_combat_stats']")
 print("- Magical: ['cast_spell', 'channel_mana', 'get_magic_stats']")
 
 # perform an attack (Create two cards The Attacker and Defender)
-attacker = EliteCard("Crown of Kings", )
+attacker = EliteCard("Crown of Kings", 7, "legendary", 10, 6)
