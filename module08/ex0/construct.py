9af2a8dcb7b47381c9b4e06001a6f6f4383c6b57
ac-kavali <ahmadachahi400@gmail.com>
2026-02-02 16:26:12 +0100

    Update

---
+++ module08/ex0/construct.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import site
+
+
+python_path = sys.executable
+prefix = sys.prefix
+base_prefix = sys.base_prefix
+
+in_venv = prefix != base_prefix
+
+if in_venv:
+    print("MATRIX STATUS: Welcome to the construct\n")
+else:
+    print("MATRIX STATUS: You're still plugged in\n")
+print(f"Current Python: {python_path}")
+
+if in_venv:
+    print(f"Virtual Environment: {os.path.basename(prefix)}")
+    print(f"Environment Path: {prefix}")
+else:
+    print("Virtual Environment: None detected")
+
+site_packages = site.getsitepackages()[0]
+print(f"\nPackage installation path:\n{site_packages}")
